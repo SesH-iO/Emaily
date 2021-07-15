ec0ddaa48f765b801b573b23670bc14baafb6319
SesH.iO <sesh.io1022@gmail.com>
2021-07-15 18:38:10 +0530

    added README.md

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
 // * Route handler
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send({ hi: 'there' }));
+
+// * Google OAuth
+// * This new GoogleStrategy creates a new instance of the Google passport strategy
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 8000; // * Dynamic PORT Binding
 app.listen(PORT);
