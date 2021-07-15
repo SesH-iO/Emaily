64ee260dfa2ab260c6e93e081395ec7983357b1e
SesH.iO <sesh.io1022@gmail.com>
2021-07-15 18:08:26 +0530

    google oauth test

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passort-google-oauth20').Strategy;
 const app = express();
 
 // * Route handler
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// * Google OAuth
-// * This new GoogleStrategy creates a new instance of the Google passport strategy
-// passport.use(new GoogleStrategy());
-
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000; // * Dynamic PORT Binding
 app.listen(PORT);
